c26f864675464ad9c8361928aa71795fbb94538b
salimalitarikz <salimalitarikz@gmail.com>
2025-04-23 00:04:54 +0300

    update_3

--- site.js
+++ site.js
@@ -2,6 +2,23 @@ fetch("nav.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("nav").innerHTML = data;
+
+    let menu = document.getElementById("menu");
+    let menuButton = document.getElementById("menuButton");
+    let body = document.querySelector("body");
+
+    menuButton.addEventListener("click", function () {
+      menu.classList.toggle("active");
+      menuButton.classList.toggle("active");
+      body.classList.toggle("stopped");
+      
+    });
+  });
+
+fetch("footer.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
   });
 
 // Lenis örneği oluştur
